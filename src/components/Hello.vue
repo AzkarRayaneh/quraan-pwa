4a2501f66203e944e5c70277db4c3794c9e42a6f
Mohammd Sharifi <mmdsharifi@yahoo.com>
2017-06-28 14:38:05 +0430

    feat(pwa): Init with Vue pwa template

---
+++ src/components/Hello.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>آرام دل💖ها</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'به برنامه قرآنی آرام دلها خوش آمدید!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
